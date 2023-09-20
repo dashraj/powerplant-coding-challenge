7b9fc6580021e0296f7d875eb6fe5742e857c131
Raja Dashnamoorthy <draja1688@gmail.com>
2023-09-21 00:18:34 +0530

    minor tweak

--- src/Core/Models/ProductionPlan.cs
+++ src/Core/Models/ProductionPlan.cs
@@ -9,7 +9,6 @@
         public double CostPerUnit { get; set; }
         public double TotalCost { get { return Math.Round(this.Power * this.CostPerUnit, 1); } }
         public double MinCostToRun { get { return Math.Round(this.MinPower * this.CostPerUnit, 1); } }
-
         public int Order { get { return this.Power > 0 ? 0 : 1; } }
     }
 
